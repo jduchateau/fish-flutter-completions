5853d10458b123eaae954bf48a4df0ecdc14ad46
Jakub Duchateau <duchateaujakub@gmail.com>
2020-10-05 18:25:46 +0200

    Completions for create, doctor, and emulators.

--- completions/flutter.fish
+++ completions/flutter.fish
@@ -1,6 +1,7 @@
-set -l commands analyse assemble attach bash-completion build channel clean config create devices doctor downgrade drive emulators format install logs precache pub run screenshot symbolize test upgrade
+# Flutter completion (v1.20.4)
 
-complete -c flutter -f -d "Manage your Flutter app development."
+## General options
+complete -c flutter -d "Manage your Flutter app development."
 
 complete -c flutter -s h -l help -d "Print usage information."
 
@@ -14,6 +15,47 @@ complete -c flutter -l suppress-analytics -d "Suppress analytics reporting when 
 
 complete -c flutter -l packages -d "Path to your '.packages' file."
 
-complete -c flutter -x -n "not __fish_seen_subcommand_from $commands" -a "$commands"
+## Commands
+set -l commands analyze assemble attach bash-completion build channel clean config create devices doctor downgrade drive emulators format install logs precache pub run screenshot symbolize test upgrade
 
-# TODO completions in subcommands
+complete -c flutter -n "not __fish_seen_subcommand_from $commands" -a "$commands"
+complete -c flutter -n "not __fish_seen_subcommand_from $commands help" -a "help"
+complete -c flutter -n "__fish_seen_subcommand_from help" -a "$commands"
+
+### flutter create
+complete -c flutter -n "__fish_seen_subcommand_from create" -l platforms
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l pub -d "Run 'flutter pub get' after the project has been created"
+complete -c flutter -n "__fish_seen_subcommand_from create" -l no-pub
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l offline -d "Run it in offline mode"
+complete -c flutter -n "__fish_seen_subcommand_from create" -l no-offline
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l with-driver-test -d "Add a flutter_driver dependency and generate a sample 'flutter drive' test"
+complete -c flutter -n "__fish_seen_subcommand_from create" -l no-with-driver-test
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -s t -l template -a "app module package plugin" -d "Specify the type of project to create"
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -s s -l sample -d "Specifies the Flutter code sample to use as the main.dart for an application"
+complete -c flutter -n "__fish_seen_subcommand_from create" -l list-samples
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l overwrite -d "Overwrite existing files"
+complete -c flutter -n "__fish_seen_subcommand_from create" -l no-overwrite
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l description -d "(defaults to 'A new Flutter project.')"
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l org -d "The organization responsible for your new Flutter project, in reverse domain name notation. (defaults to 'com.exemple')"
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -l project-name
+
+complete -c flutter -n "__fish_seen_subcommand_from create" -s i -l ios-language -d "[objc, swift (default)]"
+complete -c flutter -n "__fish_seen_subcommand_from create" -s a -l android-language -d "[objc, swift (default)]"
+
+### flutter doctor
+complete -c flutter -n "__fish_seen_subcommand_from doctor" -l android-licenses -d "Run the Android SDK manager tool to accept the SDK's licenses"
+
+
+### flutter emulators 
+complete -c flutter -n "__fish_seen_subcommand_from emulators" -l launch -d "The full or partial ID of the emulator to launch"
+complete -c flutter -n "__fish_seen_subcommand_from emulators" -l name -d "Used with flag --create. Specifies a name for the emulator being created"
+complete -c flutter -n "__fish_seen_subcommand_from emulators" -l create -d "Creates a new Android emulator based on a Pixel device"
