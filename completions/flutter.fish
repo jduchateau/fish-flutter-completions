f045d7e28df9be1dc7ea0a0cf143acf9e65b29df
Jakub Duchateau <jduchateau@student.uliege.be>
2020-11-02 16:16:05 +0100

    flutter run partial descriptions

--- completions/flutter.fish
+++ completions/flutter.fish
@@ -59,3 +59,50 @@ complete -c flutter -n "__fish_seen_subcommand_from doctor" -l android-licenses 
 complete -c flutter -n "__fish_seen_subcommand_from emulators" -l launch -d "The full or partial ID of the emulator to launch"
 complete -c flutter -n "__fish_seen_subcommand_from emulators" -l name -d "Used with flag --create. Specifies a name for the emulator being created"
 complete -c flutter -n "__fish_seen_subcommand_from emulators" -l create -d "Creates a new Android emulator based on a Pixel device"
+
+### flutter run
+complete -c flutter -n "__fish_seen_subcommand_from run" -l debug
+complete -c flutter -n "__fish_seen_subcommand_from run" -l profile
+complete -c flutter -n "__fish_seen_subcommand_from run" -l release
+complete -c flutter -n "__fish_seen_subcommand_from run" -a --dart-define= -d "Additional key-value pairs that will be available as constants from the String.fromEnvironment, bool.fromEnvironment, int.fromEnvironment, and double.fromEnvironment constructors."
+complete -c flutter -n "__fish_seen_subcommand_from run" -l flavor -d "Build a custom app flavor as defined by platform-specific build setup."
+complete -c flutter -n "__fish_seen_subcommand_from run" -l trace-stratup -d "Trace application startup, then exit, saving the trace to a file."
+complete -c flutter -n "__fish_seen_subcommand_from run" -l verbose-system-logs
+complete -c flutter -n "__fish_seen_subcommand_from run" -l cache-sksl
+complete -c flutter -n "__fish_seen_subcommand_from run" -l dump-skp-on-shader-compilation
+complete -c flutter -n "__fish_seen_subcommand_from run" -l purge-persistent-cache
+complete -c flutter -n "__fish_seen_subcommand_from run" -l route -d "Which route to load when running the app."
+complete -c flutter -n "__fish_seen_subcommand_from run" -a --vmservice-out-file= -d "A file to write the attached vmservice uri to after an application is started."
+complete -c flutter -n "__fish_seen_subcommand_from run" -l target -s t -d "The main entry-point file of the application, as run on the device."
+complete -c flutter -n "__fish_seen_subcommand_from run" -l observatory-port
+complete -c flutter -n "__fish_seen_subcommand_from run" -l device-vmservice-port
+complete -c flutter -n "__fish_seen_subcommand_from run" -l host-vmservice-port
+complete -c flutter -n "__fish_seen_subcommand_from run" -l pub
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-pub
+complete -c flutter -n "__fish_seen_subcommand_from run" -l track-widget-creation
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-track-widget-creation
+complete -c flutter -n "__fish_seen_subcommand_from run" -l null-assertions
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-null-assertions
+complete -c flutter -n "__fish_seen_subcommand_from run" -a device-user= -d "Identifier number for a user or work profile on Android only. Run `adb shell pm list users` for available identifiers."
+complete -c flutter -n "__fish_seen_subcommand_from run" -a device-timeout=10 -d "Time in seconds to wait for devices to attach. Longer timeouts may be necessary for networked devices."
+complete -c flutter -n "__fish_seen_subcommand_from run" -l start-paused
+complete -c flutter -n "__fish_seen_subcommand_from run" -l enable-software-rendering
+complete -c flutter -n "__fish_seen_subcommand_from run" -l skia-deterministic-rendering
+complete -c flutter -n "__fish_seen_subcommand_from run" -l trace-skia
+complete -c flutter -n "__fish_seen_subcommand_from run" -l endless-trace-buffer
+complete -c flutter -n "__fish_seen_subcommand_from run" -l trace-systrace
+
+complete -c flutter -n "__fish_seen_subcommand_from run" -l await-first-frame-when-tracing
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-await-first-frame-when-tracing
+
+complete -c flutter -n "__fish_seen_subcommand_from run" -l use-test-fonts
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-use-test-fonts
+
+complete -c flutter -n "__fish_seen_subcommand_from run" -l build
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-build
+
+complete -c flutter -n "__fish_seen_subcommand_from run" -l hot
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-hot
+complete -c flutter -n "__fish_seen_subcommand_from run" -l pid-file
+complete -c flutter -n "__fish_seen_subcommand_from run" -l fast-start
+complete -c flutter -n "__fish_seen_subcommand_from run" -l no-fast-start
